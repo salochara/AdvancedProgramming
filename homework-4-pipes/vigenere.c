6f03607ad0ece445fe7f117896ee3d8b17c80c77
Salo Charabati <salochara@gmail.com>
2019-10-03 15:51:24 -0500

    working. missing comments

--- homework-4-pipes/vigenere.c
+++ homework-4-pipes/vigenere.c
@@ -2,37 +2,28 @@
 // Created by Salomón Charabati on 2019-09-26.
 //
 
-/* TODO
-Wait for user input, to encode or decode message
-Wait for user input, filename that has the message
-Wait for user input, to provide key
-
- Fork the process
- The child executes viginerie
- Parent waits
- Child returns to parent the filename result.
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <ctype.h>
-#define MAX_STRING_SIZE 30
+#define MAX_STRING_SIZE 50
 #define BUFFER_SIZE 200
+#define ENCODE_OPTION 0
+#define DECODE_OPTION 1
 
 void getUserInput(char decodeEncodeOption[], char filename[], char key[]);
 void openPipes(int *parentToChild, int *childToParent);
 void printPipes(int pipeOne[], int pipeTwo[]);
 char * vigenere(char decodeEncodeOption [], char inputFileName [], char key []);
 char * vigenere_decode(char inputFileName[],char key[]);
+char * prepareFileName(char inputFileName[],int option);
 int main()
 {
     char decodeEncodeOption[MAX_STRING_SIZE], fileName[MAX_STRING_SIZE], key[MAX_STRING_SIZE];
     char * resultFileName;
+    char buffer[MAX_STRING_SIZE];
 
-    getUserInput(decodeEncodeOption, fileName, key);
 
     // Open the pipes
     int parent_to_child[2];
@@ -45,15 +36,35 @@ int main()
     pid_t new_pid;
     new_pid = fork();
 
-    resultFileName = vigenere(decodeEncodeOption, fileName, key);
+    //prepareFileName(fileName,DECODE_OPTION);
+
 
     // Identify the processes
     if(new_pid > 0) // Parent process
     {
+        getUserInput(decodeEncodeOption, fileName, key);
+        sprintf(buffer,"%s|%s|%s",decodeEncodeOption,fileName,key);
+        write(parent_to_child[1],buffer,MAX_STRING_SIZE);
+        read(child_to_parent[0],buffer,MAX_STRING_SIZE);
+        printf("buffer parent: %s\n",buffer);
 
     }else if(new_pid == 0) // Child process
     {
+        char * option, *fileNameTok, *keyTok;
+        read(parent_to_child[0],buffer,MAX_STRING_SIZE);
+        printf("buffer: %s\n",buffer);
 
+        option = strtok(buffer,"|");
+        fileNameTok = strtok(NULL, "|");
+        keyTok = strtok(NULL, "|");
+        //sscanf(buffer,"%s|%s|%s",decodeEncodeOption,fileName,key);
+        //printf("buffer2: %s|%s|%s",decodeEncodeOption,fileName,key);
+
+        printf("\naqui:%s %s %s",option,fileNameTok,keyTok);
+        fflush(stdout);
+        resultFileName = vigenere(option, fileNameTok, keyTok);
+
+        write(child_to_parent[1],"hola",MAX_STRING_SIZE);
     }
     else
     {
@@ -63,60 +74,92 @@ int main()
 
 }
 
+
+char * prepareFileName(char inputFileName[],int option)
+{
+
+   if(option == 1)
+   {
+       char encoded_[MAX_STRING_SIZE] = "encoded_";
+       strncat(encoded_,inputFileName,MAX_STRING_SIZE);
+   }else if(option == 0)
+   {
+       char decoded_[MAX_STRING_SIZE] = "decoded_";
+       strncat(decoded_,inputFileName,MAX_STRING_SIZE);
+   }
+
+}
 char * vigenere(char decodeEncodeOption [], char inputFileName [], char key [])
 {
     char * resultFileNameLocal;
+    printf("\n opcion:%s\n",decodeEncodeOption);
     // Encode or decrypt?
-    if(decodeEncodeOption[0] == 'd')
+    if(strncmp(decodeEncodeOption,"d",MAX_STRING_SIZE) == 0)
     {
+        printf("si\n");
         resultFileNameLocal = vigenere_decode(inputFileName,key);
+    }else {
+        printf("no\n");
+        // vigenere encode
     }
 
 
-    // call vigenere for encrypt or decrypt
-
-
-
-
-
-
 }
+
+// call vigenere for encrypt or decrypt
+
 char * vigenere_decode(char inputFileName[],char key[])
 {
+
     FILE * filePointer = NULL;
     char buffer[BUFFER_SIZE];
+    printf("Going to open: %s",inputFileName);
+    fflush(stdout);
+
     filePointer = fopen(inputFileName,"r");
+    if(filePointer == NULL)
+    {
+        printf("Cant find file '%s'", inputFileName);
+        perror("File error");
+
+    }
 
     FILE * fileOutputPointer = NULL;
     fileOutputPointer = fopen("AQUIII.txt", "w");
 
-    // encoded_aladdin.txt
+
     // Read what comes from the fifo file until the end
 
     int keyLength = strlen(key);
-    char decryptedMsg[MAX_STRING_SIZE];
+    int i = 0, j= 0;
+    char decryptedMsg[200];
     while (fgets(buffer, BUFFER_SIZE, filePointer))
     {
         size_t sizeOfBuffer = strlen(buffer);
-        for (int i = 0, j = 0; i < sizeOfBuffer ; ++i, ++j)
+        printf("\n key at beggining of line: [%c] of length %d",key[j%keyLength], sizeOfBuffer);
+        for (i = 0; i < sizeOfBuffer ; ++i, ++j)
         {
-            if(j == keyLength)
-                j = 0;
+            /*if(j == keyLength)
+                j = 0;*/
 
             if(!isalpha(buffer[i])){
-                buffer[i] = buffer[i];
+               // buffer[i] = buffer[i];
                 decryptedMsg[i] = buffer[i]; // for decrypt
                 continue;
             }
             if(isalpha(buffer[i])){
                 //encryptedMsg[i] = (  (msg[i] - 'a' + key[j] - 'a') % 26 )  + 'a'; // for encrypt
-                int letter = (( buffer[i] -'a')  - (key[j] -'a') + 26 ) % 26; // for decryot
+                int letter = (( buffer[i] -'a')  - (key[j % keyLength] -'a') + 26 ) % 26; // for decryot
                 decryptedMsg[i] = letter + 'a';
-                buffer[i] = letter +'a';
+                //buffer[i] = letter +'a';
             }
+            //printf("%s\n",buffer);
         }
-        fprintf(fileOutputPointer,"%s",buffer);
-        printf("\n%s\n",buffer);
+        decryptedMsg[i] ='\0';
+        fprintf(fileOutputPointer,"%s",decryptedMsg);
+        //printf("\n%s\n",decryptedMsg);
+
+        fflush(stdout);
     }
 
     fclose(filePointer);
@@ -141,21 +184,19 @@ void openPipes(int *parentToChild, int *childToParent)
 
 }
 
-
 // Get user input needed for using the program
 void getUserInput(char * decodeEncodeOption, char * filename, char * key)
 {
-   printf("Do you want to encode or decode?(d/e): \t");
+   //printf("Do you want to encode or decode?(d/e): \t");
    strncpy(decodeEncodeOption, "d", MAX_STRING_SIZE);
    //scanf("%s", decodeEncodeOption);
 
-   printf("Type the filename:\t");
+   //printf("Type the filename:\t");
    strncpy(filename,"encoded_aladdin.txt",MAX_STRING_SIZE);
    //scanf("%s",filename);
 
-   printf("Type the key:\t");
+   //printf("Type the key:\t");
    strncpy(key,"prograavanzada",MAX_STRING_SIZE);
    //scanf("%s",key);
-
 }
 
